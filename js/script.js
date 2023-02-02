460f4a3fadb2c6ffbfc3a7591e5689f2d17a3731
ramoncodevale <ramonvalentim88@gmail.com>
2023-02-02 15:12:19 -0300

    add menu

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@ window.addEventListener("scroll", function(){
 });
 
 
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+
+
+}
